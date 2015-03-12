c0ca8ddcfa7729399de6f5a48a5c2c6695d15d06
dwaybright <daniel.waybright@gmail.com>
2015-03-11 22:08:26 -0600

    Small Changes

--- core/src/us/thirdmillenium/strategicassaultsimulator/agents/PuppetAgent.java
+++ core/src/us/thirdmillenium/strategicassaultsimulator/agents/PuppetAgent.java
@@ -26,46 +26,72 @@
 
 package us.thirdmillenium.strategicassaultsimulator.agents;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ai.pfa.DefaultGraphPath;
+import com.badlogic.gdx.ai.pfa.GraphPath;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 
+import java.util.HashMap;
+
+import us.thirdmillenium.strategicassaultsimulator.ai.TileAStarPathFinder;
+import us.thirdmillenium.strategicassaultsimulator.ai.TileHeuristic;
+import us.thirdmillenium.strategicassaultsimulator.ai.TileNode;
 import us.thirdmillenium.strategicassaultsimulator.brains.Brain;
 import us.thirdmillenium.strategicassaultsimulator.brains.PuppetBrain;
 
 
 public class PuppetAgent extends AgentModel {
     // Game World
-    TiledMapTileLayer CollisionLayer;
-    Sprite Sprite;
-    Texture Alive;
-    Texture Dead;
+    private TiledMapTileLayer CollisionLayer;
+    private Sprite Sprite;
+    private Texture Alive;
+    private Texture Dead;
+    private int TileSize;
+    private HashMap<Integer, TileNode> MapNodes;
+    private TileAStarPathFinder PathFinder;
 
+    // Agent location
+    private GraphPath<TileNode> CurrentPath;
+    private float Pixel_X;
+    private float Pixel_Y;
+    private int Cell_X;
+    private int Cell_Y;
+    private float Angle;
 
     // Agent Physical Attributes
     private int Health;
     private float MovementSpeed;
     private float Eyesight;
     private float Hearing;
-    private float Angle;
-
 
     // Agent Equipment
     //private WeaponModel MyWeapon;
     //private ArmorModel MyArmor;
 
-
     // Agent Mental Model
     private Brain Control;
 
 
-    public PuppetAgent(TiledMapTileLayer collisionLayer)
+
+    public PuppetAgent(TiledMapTileLayer collisionLayer, HashMap<Integer, TileNode> mapNodes,
+                       TileAStarPathFinder pathFinder,
+                       int pixelX, int pixelY, int tileSize)
     {
         // Setup game world parameters
         this.CollisionLayer = collisionLayer;
+        this.TileSize = tileSize;
+        this.MapNodes = mapNodes;
+        this.PathFinder = pathFinder;
+
+        // Setup Graphics
         this.Alive = new Texture("goodGuyDot.png");
         this.Dead = new Texture("deadDot.png");
         this.Sprite = new Sprite(Alive);
+
+        // Setup Start Location
+
 
         // Set Basic Values
         this.Health = 50;
@@ -86,5 +112,49 @@ public class PuppetAgent extends AgentModel {
     @Override
     public void updateAgentState() {
 
+    }
+
+
+    private GraphPath<TileNode> getPathToTouch(float goalX, float goalY) {
+        // Generate Cell X and Y positions
+        int numCellY = Gdx.graphics.getHeight() / this.TileSize;
+
+        float spriteX = this.Sprite.getX();
+        float spriteY = this.Sprite.getY();
+
+        int spriteCellX = (int) (spriteX / this.TileSize);
+        int spriteCellY = (int) (spriteY / this.TileSize);
+
+        int touchCellX = (int) (goalX / this.TileSize);
+        int touchCellY = (int) (goalY / this.TileSize);
+
+        // Start node and connections
+        //TileNode startNode = new TileNode(spriteX,spriteY,spriteCellX,spriteCellY,950);
+        TileNode startNode = findIndex((spriteCellX * numCellY) + spriteCellY, this.MapNodes);
+
+        // End node and connections
+        //TileNode endNode = new TileNode(touchX, touchY, touchCellX, touchCellY, 951);
+        TileNode endNode = findIndex((touchCellX * numCellY) + touchCellY, this.MapNodes);
+
+        // The returned path once computed
+        //GraphPath<Connection<TileNode>> path = new DefaultGraphPath<Connection<TileNode>>();
+        this.CurrentPath = new DefaultGraphPath<TileNode>();
+
+        // Compute Path!
+        //this.indexedPathFinder.searchConnectionPath(startNode, endNode, new TileHeuristic(), path);
+        this.PathFinder.searchNodePath(startNode, endNode, new TileHeuristic(), this.CurrentPath);
+
+
+        return path;
+    }
+
+    private TileNode findIndex(Integer index, HashMap<Integer, TileNode> nodeTracker) {
+        TileNode temp = null;
+
+        if(nodeTracker.containsKey(index)) {
+            temp = nodeTracker.get(index);
+        }
+
+        return temp;
     }
 }
