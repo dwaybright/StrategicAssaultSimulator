daa36caee3e53dd9f9394b06fd0d7e98e438b50c
Daniel Waybright <daniel.waybright@gmail.com>
2015-05-04 14:10:53 -0600

    Added images for startup screen

--- core/src/us/thirdmillenium/strategicassaultsimulator/startup/Startup.java
+++ core/src/us/thirdmillenium/strategicassaultsimulator/startup/Startup.java
@@ -20,19 +20,25 @@ import android.content.res.AssetManager;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
 
 
 public class Startup implements InputProcessor {
     // Sprite Drawing
     private OrthographicCamera camera;
     private SpriteBatch spriteBatch;
+    private ShapeRenderer shapeRenderer;
 
-    // Sprites for levels
+    // Level Variables
+    private Rectangle[] levelSelectRects;
+    private int chosenLevel = 1;
     private Sprite chooseLevelText;
     private Sprite level1;
     private Sprite level2;
@@ -41,14 +47,19 @@ public class Startup implements InputProcessor {
     private Sprite level5;
 
 
+
     public Startup(AssetManager assman) {
         // Setup camera
         this.camera = new OrthographicCamera();
         this.camera.setToOrtho(false, 800, 1216);
         this.camera.update();
 
+        // Setup input (motion grabbing) processing
+        Gdx.input.setInputProcessor(this);
+
         // Setup SpriteBatch
         this.spriteBatch = new SpriteBatch();
+        this.shapeRenderer = new ShapeRenderer();
 
         // Load Level Images
         this.chooseLevelText = new Sprite(new Texture("LevelImages/ChooseLevel.png"));
@@ -61,11 +72,23 @@ public class Startup implements InputProcessor {
         // Set Level Image Locations
         this.chooseLevelText.setX(50);
         this.chooseLevelText.setY(1100);
-        this.level1.setCenter(100, 1050);
-        this.level2.setCenter(250, 1050);
-        this.level3.setCenter(400, 1050);
-        this.level4.setCenter(550, 1050);
-        this.level5.setCenter(700, 1050);
+        this.level1.setCenter(100, 1000);
+        this.level2.setCenter(250, 1000);
+        this.level3.setCenter(400, 1000);
+        this.level4.setCenter(550, 1000);
+        this.level5.setCenter(700, 1000);
+
+        // Create Level Selection Rectangles
+        this.levelSelectRects = new Rectangle[5];
+
+        for( int i = 0; i < 5; i ++ )
+            this.levelSelectRects[i] = new Rectangle();
+
+        this.levelSelectRects[0].set( 30, 904, 140, 192);
+        this.levelSelectRects[1].set(180, 904, 140, 192);
+        this.levelSelectRects[2].set(330, 904, 140, 192);
+        this.levelSelectRects[3].set(480, 904, 140, 192);
+        this.levelSelectRects[4].set(630, 904, 140, 192);
     }
 
 
@@ -76,7 +99,41 @@ public class Startup implements InputProcessor {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         this.camera.update();
 
-        // Draw Level Thumb Images
+        // Draw Level Info
+        drawLevelInfo();
+
+        // Draw Player Type Selection
+
+
+        // Draw Enemy Type Selection
+
+
+        // Draw Ready for Path Selection
+
+    }
+
+    private void drawLevelInfo() {
+        // Draw Level Selection Indicators
+        this.shapeRenderer.setProjectionMatrix(this.camera.combined);
+        this.shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+
+        for(int i = 0; i < 5; i++ ) {
+
+            if( this.chosenLevel == i ) {
+                this.shapeRenderer.setColor(Color.GREEN);
+            } else {
+                this.shapeRenderer.setColor(Color.BLACK);
+            }
+
+            this.shapeRenderer.rect(this.levelSelectRects[i].getX(),
+                                   this.levelSelectRects[i].getY(),
+                                   this.levelSelectRects[i].getWidth(),
+                                   this.levelSelectRects[i].getHeight());
+        }
+
+        this.shapeRenderer.end();
+
+        // Draw Level Thumb Images and Text
         this.spriteBatch.setProjectionMatrix(this.camera.combined);
         this.spriteBatch.begin();
 
@@ -107,7 +164,35 @@ public class Startup implements InputProcessor {
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        return false;
+        System.out.println(screenX + ", " + screenY);
+
+        int x = screenX;
+        int y = 1216 - screenY;
+
+        // Check for Level Select Collision
+        for(int i = 0; i < 5; i++) {
+            if( x > this.levelSelectRects[i].getX()
+                && x < this.levelSelectRects[i].getX() + this.levelSelectRects[i].getWidth()
+                && y > this.levelSelectRects[i].getY()
+                && y < this.levelSelectRects[i].getY() + this.levelSelectRects[i].getHeight())
+            {
+                this.chosenLevel = i;
+            }
+        }
+
+        // Check for Player Type Selection
+
+
+
+        // Check for Enemy Type Selection
+
+
+
+        // Check for Go To Path Selection button
+
+
+
+        return true;
     }
 
     @Override
