8821fe596876499116ad1cdd3e1edc5c577a3347
Daniel Waybright <daniel.waybright@gmail.com>
2015-05-04 10:27:09 -0600

    Ready to render level images on startup screen

--- core/src/us/thirdmillenium/strategicassaultsimulator/startup/Startup.java
+++ core/src/us/thirdmillenium/strategicassaultsimulator/startup/Startup.java
@@ -1,16 +1,32 @@
+/*
+ Copyright (C) 2015 Daniel Waybright, daniel.waybright@gmail.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
 package us.thirdmillenium.strategicassaultsimulator.startup;
 
 import android.content.res.AssetManager;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-/**
- * Created by danielwaybright on 5/4/15.
- */
+
 public class Startup implements InputProcessor {
     // Sprite Drawing
     private OrthographicCamera camera;
@@ -39,11 +55,34 @@ public class Startup implements InputProcessor {
         this.level3 = new Sprite(new Texture("LevelImages/Small_Level3.png"));
         this.level4 = new Sprite(new Texture("LevelImages/Small_Level4.png"));
         this.level5 = new Sprite(new Texture("LevelImages/Small_Level5.png"));
+
+        // Set Level Image Locations
+        this.level1.setCenter(100, 1100);
+        this.level2.setCenter(250, 1100);
+        this.level3.setCenter(400, 1100);
+        this.level4.setCenter(550, 1100);
+        this.level5.setCenter(700, 1100);
     }
 
 
     public void draw() {
-        
+        // Clear Background, and update camera
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        this.camera.update();
+
+        // Draw Level Thumb Images
+        this.spriteBatch.setProjectionMatrix(this.camera.combined);
+        this.spriteBatch.begin();
+
+        this.level1.draw(this.spriteBatch);
+        this.level2.draw(this.spriteBatch);
+        this.level3.draw(this.spriteBatch);
+        this.level4.draw(this.spriteBatch);
+        this.level5.draw(this.spriteBatch);
+
+        this.spriteBatch.end();
     }
 
     @Override
